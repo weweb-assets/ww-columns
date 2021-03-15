95a3c7b08f9d64db7260d0a37ae2d7ee17aea302
Aurélie VIOLETTE <orwel.violette@gmail.com>
2021-03-15 09:37:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Element component starter
+
+This is an element for [weweb.io](https://www.weweb.io/).
+
+## Installation
+
+To run locally, first install all dependencies with `yarn` or `npm i`
+
+## Start
+
+To serve locally, run `yarn serve --port=[PORT]` or `npm run serve --port=[PORT]`, and then go to Weweb editor, open developper popup and add your custom element.
+
+## Build
+Before release, you can check build error by running `yarn build --name=my-element`
---
+++ src/wwElement.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="my-element">
+        <p :style="textStyle">I am a custom element !</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "__COMPONENT_NAME__",
+    wwDefaultContent: {
+        textColor: "#F23636",
+    },
+    props: {
+        content: { type: "Object", required: true },
+    },
+    computed: {
+        textStyle() {
+            return {
+                color: this.content.textColor,
+            };
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.my-element {
+    p {
+        font-size: 18px;
+    }
+}
+</style>
